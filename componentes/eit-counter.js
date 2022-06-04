c9dbfff6da943db49b14edc9ea0d4e9f6332aa29
applejuanm <74629411+applejuanm@users.noreply.github.com>
2022-06-04 11:01:43 +0200

    parte de intro a lit

--- componentes/eit-counter.js
+++ componentes/eit-counter.js
@@ -12,7 +12,15 @@ export class EitCounter extends LitElement {
     }
     .parrafo {
       color: blue;
-      font-size: 1.5rem;
+      font-size: 1.5em;
+            }
+    input {
+      width: 30px;
+    }
+    @media(min-width: 500px) {
+      .parrafo {
+        font-size: 3em;
+      }
     }
   `;
   static get properties() {
@@ -26,9 +34,22 @@ export class EitCounter extends LitElement {
   }
   render() {
     return html`
+      <slot></slot>
       <h2>Mi contador</h2>
+      <input id="quantity" type="number" />
       <p class="parrafo">${this.counter}</p>
+      <button @click="${this.incrementar}">+1</button>
+      <button @click="${this.decrementar}">-1</button>
     `;
+  }
+
+  incrementar() {
+    let quantity = this.shadowRoot.getElementById('parrafo').value;
+    this.counter++;
+  }
+
+  decrementar() {
+    this.counter--;
   }
 }
 customElements.define('eit-counter', EitCounter);
